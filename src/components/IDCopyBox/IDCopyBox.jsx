66da6aa8b02aec9366f6245b1e900d88dd052e35
styv <styv2301@gmail.com>
2024-07-19 23:38:54 -0500

    add loading if id not ready

--- src/components/IDCopyBox/IDCopyBox.jsx
+++ src/components/IDCopyBox/IDCopyBox.jsx
@@ -18,16 +18,35 @@ export default function IDCopyBox({ idPeer, className }) {
     return (
         <div className={`id-copy-box${className ? ' ' + className : ''}`}>
             <p>ID :</p><p> {idPeer || '...espere generando'}</p>
-            <span onClick={(e) => handleCopy(e)}>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-copy" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-                </svg>
-                <div className="id-copy-box__shoot-copy">
-                    Copy!
-                </div>
-            </span>
+            {idPeer ?
+                <span onClick={(e) => handleCopy(e)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-copy" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                        <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+                    </svg>
+                    <div className="id-copy-box__shoot-copy">
+                        Copy!
+                    </div>
+                </span> :
+                <span>
+                    <svg style={{ animation: "giveColor 1.5s forwards,rotateIcon 0.8s infinite linear" }}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="icon icon-tabler icons-tabler-outline icon-tabler-loader-2">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 3a9 9 0 1 0 9 9" />
+                    </svg>
+                </span>
+            }
+
         </div>
     )
 }
