7a32099c6c910e3df388c0b4e85d3347cacc769e
styv <styv2301@gmail.com>
2026-02-05 23:58:45 -0500

    feat: añadir interface global para window toast

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,18 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export {};
+declare global {
+  interface Window {
+    toast: (options: {
+      title?: string;
+      message: string;
+      type?: string;
+      location?: string;
+      icon?: string;
+      theme?: string;
+      customIcon?: string;
+      dismissable?: boolean;
+    }) => void;
+  }
+}
